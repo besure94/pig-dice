86c83552aac7c086b028944fc3a87e09c7b02a3e
Brian Scherner <brianscherner@gmail.com>
2023-06-29 15:55:05 -0700

    Write Player and Game constructor and method for addName to player.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,37 @@ let rollDice = Math.floor(Math.random() * 6) +1;
 
 console.log("You rolled a", rollDice);
 
-// function Player(turn) {
-// 	this.roll = 0;
-// 	this.currentScore = 0;
-// 	this.totalScore = 0;
-// 	this.turn = turn;
+function Player() {
+	this.playerName = "";
+	this.score = 0;
+}
+
+Player.prototype.addName = function(userName) {
+	this.playerName = userName;
+}
+
+let player = new Player();
+
+player.addName("Bob");
+console.log(player);
+
+function Game() {
+	this.currentPlayerTurn = "";
+}
+
+let game = new Game();
+
+console.log(game);
+
+// let player = new Player();
+
+// Player.prototype.rollOne = function() {
+// 	if (this.roll === 1) {
+// 		this.turnScore = 0;
+// 		return "You rolled a 1. Your turn is over!";
+// 	} else {
+// 		this.turnScore += this.roll;
+// 	}
 // }
+
+// console.log();
