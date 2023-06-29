0889777c50762a1306b6bf52b0595ecc71bdf7d3
Brian Scherner <brianscherner@gmail.com>
2023-06-29 11:04:47 -0700

    Write function for dice roll.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
-function PigDice() {
-	this.player1 = {};
-	this.player2 = {};
-}
+// function Dice() {
+// 	this.dice = 1;
+// 	this.diceValue = 0;
+// }
 
-function Player(turn) {
-	this.roll = 0;
-	this.currentScore = 0;
-	this.totalScore = 0;
-	this.turn = turn;
-}
+let rollDice = Math.floor(Math.random() * 6) +1;
+
+console.log("You rolled a", rollDice);
+
+// function Player(turn) {
+// 	this.roll = 0;
+// 	this.currentScore = 0;
+// 	this.totalScore = 0;
+// 	this.turn = turn;
+// }
